92b33ed797f84a241c4fd0588c143b58b9fa7996
Alexio <alexmota@eden.rutgers.edu>
2013-01-19 13:54:35 -0800

    Css Change 1

--- index.php
+++ index.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="stylesheets/app.css">
+		<link rel="stylesheet" href="stylesheets/main.css">
 		<link rel="stylesheet" href="stylesheets/foundation.min.css">
 		<script src="javascripts/foundation.min.js"></script>
 	</head>
 	<body>
-
-		<?php ?>
-		<div class="row">
-		    <div class="three columns">
-		      <h1><img src="http://placehold.it/400x100&text=Logo" /></h1>
-		    </div>
-		    <div class="nine columns">
-		      <ul class="nav-bar right">
-		        <li><a href="#">Your Name</a></li>
-		        <li><a href="#">Scheduler</a></li>
-		        <li><a href="#">Logout</a></li>
-		      </ul>
-		    </div>
-		</div>
-
-		<div class="row">
-
-			<div class="five columns">
-				<div row>
-					<!--Group Search Box!-->
-					<p>Search box</p>
-				</div>
+		<div class="container">
+			<div class="row">
+			    <div class="three columns">
+			      <h1><img src="Logo.png" /></h1>
+			    </div>
+			    <div class="nine columns">
+			      <ul class="nav-bar right">
+			        <li><a href="#">Your Name</a></li>
+			        <li><a href="#">Scheduler</a></li>
+			        <li><a href="#">Logout</a></li>
+			      </ul>
+			    </div>
 			</div>
-			<div class="seven columns">
-				<div class=row>
-					<div class="one columns">
-						<label for="checkbox1"><input type="checkbox" id="checkbox1" style="display:none;" class="support attribute"> Person's Name</label> 
+
+			<div class="row">
+
+				<div class="five columns">
+					<div row>
+						<!--Group Search Box!-->
+						<p>Search box</p>
 					</div>
 				</div>
-			
+				<div class="seven columns">
+					<div class=row>
+						<div class="one columns">
+							<label for="checkbox1"><input type="checkbox" id="checkbox1" style="display:none;" class="support attribute"> Person's Name</label> 
+						</div>
+					</div>
+				
+				</div>
 			</div>
 		</div>
 	</body>
