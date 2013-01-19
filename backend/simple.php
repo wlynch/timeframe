e3efcb10fd95e12f205736cfc35988cbb1518380
ekamp <ekamp@eden.rutgers.edu>
2013-01-19 00:54:21 -0500

    Added functionality for connecting to site

--- backend/simple.php
+++ backend/simple.php
@@ -9,10 +9,12 @@ $client->setApplicationName("Google Calendar PHP Starter Application");
 
 // Visit https://code.google.com/apis/console?api=calendar to generate your
 // client id, client secret, and to register your redirect uri.
-// $client->setClientId('insert_your_oauth2_client_id');
-// $client->setClientSecret('insert_your_oauth2_client_secret');
-// $client->setRedirectUri('insert_your_oauth2_redirect_uri');
-// $client->setDeveloperKey('insert_your_developer_key');
+
+
+$client->setClientId('client_id');
+$client->setClientSecret('client_secret');
+$client->setRedirectUri('uri');
+$client->setDeveloperKey('api_key');
 $cal = new Google_CalendarService($client);
 if (isset($_GET['logout'])) {
   unset($_SESSION['token']);
